9f8516f4d8e6d1feb266419f8e3779dca1b75a09
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2022-07-27 21:59:14 -0300

    feito

---
+++ exercicio1/script.js
@@ -0,0 +1,22 @@
+//Crie 3 arrays diferentes
+
+//Array numeros
+
+const numeros = [3, 5, 1, 2, 6, 4];
+
+//Array strings
+
+const strings = ["luva", "pedra", "lua", "flores"];
+
+//Array mista
+
+const mista = [30, "Tibas", true];
+
+//Imprimindo
+
+/*console.log(numeros,
+    strings,
+    mista);*/
+console.log(numeros);
+console.log(strings);
+console.log(mista);
